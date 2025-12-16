8c9956505f40f2f07dce1425637cc0abfcf799bd
Borys Tymchenko <tim4bor@gmail.com>
2025-12-16 21:44:43 +0200

    vsnprintf can return -1 and cause memory overflow later

--- src/remote_scales.cpp
+++ src/remote_scales.cpp
@@ -15,6 +15,11 @@ void RemoteScales::log(std::string msgFormat, ...) {
   int length = vsnprintf(nullptr, 0, msgFormat.c_str(), args); // Find length of string
   va_end(args); // End before restarting
 
+  if (length < 0) {
+    logCallback("Scale[" + device.getName() + "] Error: " + std::string(strerror(errno)));
+    return;
+  }
+
   va_start(args, msgFormat); // Restart for the actual printing
   std::string formattedMessage(length + 1, '\0'); // Instantiate formatted strigng with correct length
   vsnprintf(&formattedMessage[0], length + 1, msgFormat.c_str(), args); // print formatted message in the string
